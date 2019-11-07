95abe8eee439adf91c5f0f77369083fd38fce266
Aditya Shetty <aditya.shetty@niiconsulting.com>
2019-11-07 20:34:12 +0530

    added forms & views

--- blog_project/blog_app/models.py
+++ blog_project/blog_app/models.py
@@ -24,7 +24,7 @@ class Post(models.Model):
         return self.title
 
 
-class Comments(models.Model):
+class Comment(models.Model):
     post = models.ForeignKey('blog_app.Post', related_name='comments',
                              on_delete=models.CASCADE)
     author = models.CharField(max_length=200)
--- blog_project/blog_app/views.py
+++ blog_project/blog_app/views.py
@@ -1,3 +1,55 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Post, Comment
+from .forms import PostForm, CommentForm
 
-# Create your views here.
+from django.views.generic import (TemplateView, DeleteView, DetailView,
+                                  CreateView, ListView, UpdateView)
+
+from django.urls import reverse_lazy
+from django.contrib.auth.mixins import LoginRequiredMixin
+
+
+class AboutView(TemplateView):
+    template_name = 'blog/about.html'
+
+
+class PostListView(ListView):
+    model = Post
+
+    def get_queryset(self):
+        return Post.objects.filter(
+            published_date__lte=timezone.now()).order_by('-published_date')
+
+
+class PostDetailView(DetailView):
+    model = Post
+
+
+class PostCreateView(LoginRequiredMixin, CreateView):
+    login_url = '/login/'
+    redirect_field_name = 'blog/post_detail.html'
+    model = Post
+    form_class = PostForm
+
+
+class PostUpdateView(LoginRequiredMixin, UpdateView):
+    login_url = '/login/'
+    redirect_field_name = 'blog/post_detail.html'
+    model = Post
+    form_class = PostForm
+
+
+class PostDeleteView(LoginRequiredMixin, DeleteView):
+    model = Post
+    success_url = reverse_lazy('post_list')
+
+
+class DraftListView(LoginRequiredMixin, ListView):
+    login_url = '/login/'
+    redirect_field_name = 'blog/post_draft_list.html'
+    model = Post
+
+    def get_queryset(self):
+        return Post.objects.filter(published_date__isnull=True).order_by(
+            'created_date')
